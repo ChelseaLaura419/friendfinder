a5f4264beca55dd2a455cebbabfa1a1b6eb99cef
Chelsea Laura <cdesrosiers419@gmail.com>
2019-03-03 14:30:17 -0500

    Front-end

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var userData = [
+	{
+		name: 'Tyrion Lannister',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjM5OTYzNzQwM15BMl5BanBnXkFtZTgwMzU5MjYzNzM@._V1_.jpg',
+		scores: [
+			'3',
+			'3',
+			'2',
+			'2',
+			'3',
+			'3',
+			'3',
+			'4',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Cersei Lannister',
+		photo: 'https://m.media-amazon.com/images/M/MV5BNTgwNzQwNjgzNl5BMl5BanBnXkFtZTgwMDU5MjYzNzM@._V1_.jpg',
+		scores: [
+			'2',
+			'5',
+			'1',
+			'4',
+			'2',
+			'1',
+			'5',
+			'2',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Daenerys Targaryen',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTU2OTMzODU1MV5BMl5BanBnXkFtZTgwNTY4NDEzMzI@._V1_SY1000_CR0,0,1502,1000_AL_.jpg',
+		scores: [
+			'5',
+			'3',
+			'1',
+			'4',
+			'3',
+			'5',
+			'5',
+			'3',
+			'2',
+			'3'
+		]
+	},
+	{
+		name: 'Jon Snow',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMzQ3NDYxNzEwNV5BMl5BanBnXkFtZTgwNzQ5MjYzNzM@._V1_.jpg',
+		scores: [
+			'1',
+			'4',
+			'4',
+			'4',
+			'5',
+			'5',
+			'5',
+			'3',
+			'3',
+			'2'
+		]
+	},
+	{
+		name: 'Sansa Stark',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTY2MjA3ODc4OV5BMl5BanBnXkFtZTgwNDA0NDEzMzI@._V1_SY1000_CR0,0,1503,1000_AL_.jpg',
+		scores: [
+			'1',
+			'3',
+			'2',
+			'2',
+			'5',
+			'4',
+			'3',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Arya Stark',
+		photo: 'https://m.media-amazon.com/images/M/MV5BOTgzNzEwNDY0NF5BMl5BanBnXkFtZTgwMjQ5MjYzNzM@._V1_SY1000_CR0,0,762,1000_AL_.jpg',
+		scores: [
+			'1',
+			'4',
+			'5',
+			'5',
+			'5',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Jaime Lannister',
+		photo: 'https://m.media-amazon.com/images/M/MV5BODkxNjg1OTEyM15BMl5BanBnXkFtZTgwMzQ5MjYzNzM@._V1_.jpg',
+		scores: [
+			'3',
+			'5',
+			'5',
+			'3',
+			'2',
+			'2',
+			'3',
+			'3',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Ygritte',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjI0NjM2ODU2NV5BMl5BanBnXkFtZTgwMTQyNTI1MTE@._V1_SY1000_CR0,0,1503,1000_AL_.jpg',
+		scores: [
+			'1',
+			'1',
+			'2',
+			'3',
+			'5',
+			'4',
+			'4',
+			'2',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Theon Greyjoy',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTk2NjExNTY4OV5BMl5BanBnXkFtZTgwOTkzODMzMzI@._V1_SX1500_CR0,0,1500,999_AL_.jpg',
+		scores: [
+			'3',
+			'1',
+			'3',
+			'1',
+			'4',
+			'2',
+			'1',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Samwell Tarly',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjIxMTU2NDI1MV5BMl5BanBnXkFtZTgwMTQ5MjYzNzM@._V1_.jpg',
+		scores: [
+			'1',
+			'3',
+			'1',
+			'4',
+			'4',
+			'3',
+			'2',
+			'5',
+			'3',
+			'5'
+		]
+	}
+];
+
+module.exports = userData;
