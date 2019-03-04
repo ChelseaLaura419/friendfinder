4e1a88c238c99c6b409c9329f90068ea384717cc
Chelsea Laura <cdesrosiers419@gmail.com>
2019-03-04 17:59:05 -0500

    Server Update

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 
 
 var app = express();
@@ -7,10 +6,8 @@ var app = express();
 var PORT = process.env.PORT || 6060;
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 require("./app/routing/apiRoutes")(app);
